6c17cc08ffcdb4ed34851d3a90bcba79662c17e0
deniskhan22bd <odinstrelok02@gmail.com>
2023-09-26 15:02:04 +0600

    c

--- lab01/src/problem2/Player.java
+++ lab01/src/problem2/Player.java
@@ -2,25 +2,24 @@ package problem2;
 
 public class Player{
     private final static int MAX_LEVEL = 100;
+    private final static int MIN_LEVEL = 0;
     private final int id;
     private static int numOfPlayers;
     private String userName;
     private Rank rank;
     private int level;
-    
+
     {
         id = ++numOfPlayers;
-        rank = Rank.Newbie;
-        level = 0;
+        rank = Rank.F;
+        level = MIN_LEVEL;
     }
 
-    // public Player(){
-    //     rank = Rank.Newbie;
-    //     level = 0;
-    // }
+    public Player(){
+    
+    }
 
     public Player(String userName){
-        //this();
         this.userName = userName;
     }
 
@@ -31,21 +30,21 @@ public class Player{
     }
 
     private Rank setRank(){
-        if(level <= 10) return Rank.Newbie;
-        else if(level <= 30) return Rank.Student;
-        else if(level <= 50) return Rank.Specialist;
-        else if(level <= 70) return Rank.Expert;
-        else if(level < 100) return Rank.Master;
-        else return Rank.Grandmaster; 
+        if(level <= 10) return Rank.F;
+        else if(level <= 30) return Rank.E;
+        else if(level <= 50) return Rank.D;
+        else if(level <= 70) return Rank.C;
+        else if(level < 100) return Rank.B;
+        else return Rank.A; 
     }
 
-    public void KillEnemy(int n){
-        if(level + n > MAX_LEVEL){
+    public void KillEnemy(int numOfKilledEnemy){
+        if(level + numOfKilledEnemy > MAX_LEVEL){
             level = 100;
             System.out.println("You reach max level. Congratulation!!!!!!!\nWait for future updates");
             return;
         }
-        level += n;
+        level += numOfKilledEnemy;
         rank = setRank();
     }
 
@@ -65,16 +64,12 @@ public class Player{
         return rank;
     }
 
-    public int level(){
+    public int getLevel(){
         return level;
     }
 
     public String toString(){
         return "Id: " + id + ", username: " + userName + ", rank: " + rank + ", level: " + level + ",\nTotal num of players: " + numOfPlayers;
     }
-
-
-
-
 
 }
--- lab01/src/problem2/Rank.java
+++ lab01/src/problem2/Rank.java
@@ -1,5 +1,5 @@
 package problem2;
 
 public enum Rank{
-    Newbie, Student, Specialist, Expert, Master, Grandmaster;
+    F, E, D, C, B, A;
 }
